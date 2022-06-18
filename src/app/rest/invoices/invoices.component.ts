c1f1a6e268e24aed368b949040250e4f5302a13e
Balázs Hermann <Balazs.Hermann@awacademy.training>
2022-06-18 14:43:32 +0200

    updated invoices page

--- src/app/rest/invoices/invoices.component.ts
+++ src/app/rest/invoices/invoices.component.ts
@@ -27,14 +27,7 @@ export class InvoicesComponent implements OnInit {
     );
   }
 
-  testItNow(start : string) {
-    console.log(start);
-    let startDate = new Date(start).toISOString().slice(0, 10);
-    // startDate.getDate;
-    console.log(startDate);
-  }
-
-  getInvoicesBetweenDatesByCDate(start : string, end : string) {
+  getInvoicesBetweenDatesByComplDate(start : string, end : string) {
     // let invoicesByComplDate;
     this.servInvoices.getInvoicesBetweenDatesByCompletionDate(start, end).subscribe(invoicesByDate => {
       this.invoicesByComplDate = invoicesByDate.invoices;
@@ -50,9 +43,14 @@ export class InvoicesComponent implements OnInit {
   
   filterName1!:string;
   filterName2!:string;
+
   resetTable() {
     this.servInvoices.getInvoices().subscribe(invoices => this.listInvoices = invoices.invoices);
     this.filterName1 = '';
     this.filterName2 = '';
+  }
+  isHidden:boolean = true;
+  setFilterVisible() {
+    this.isHidden = false;
   }
 }
